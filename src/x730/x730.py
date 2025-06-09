d353730db8e0b8fa351c21d153af66606ecd359d
brunothg <android.bruns@gmail.com>
2025-06-09 18:20:52 +0200

    x730 init pins

--- src/x730/x730.py
+++ src/x730/x730.py
@@ -16,15 +16,16 @@ class X730:
     """
     Class for controlling the X730 expansion board.
 
-    Shutdown sequence: 1-2 sec for reboot, 3-7 for poweroff (default) 8+ crash (pull the plug)
+    Shutdown button sequence: 1-2 sec for reboot, 3-7 for poweroff (default) 8+ crash (pull the plug)
     """
 
     REBOOT_PULSE_MINIMUM = 200
     REBOOT_PULSE_MAXIMUM = 600
 
     def __init__(self):
-        pass
-        # TODO init pins
+        self._shutdown_button_pin = DigitalOutputDevice(pin=Pins.BUTTON.value, active_high=True, initial_value=False)
+        self._shutdown_status_pin = DigitalInputDevice(pin=Pins.SHUTDOWN.value, pull_up=False, bounce_time=None)
+        self._boot_status_pin = DigitalOutputDevice(pin=Pins.BOOT.value, active_high=True, initial_value=True)
 
     def poweroff(self, force: bool = False) -> None:
         """
