bc53877a7a108c7702e403804f74af1e6ffb8a0c
brunothg <android.bruns@gmail.com>
2025-06-16 18:57:26 +0200

    prep unit tests

---
+++ src/tests/test_x730.py
@@ -0,0 +1,8 @@
+import unittest
+
+
+class X730TestCase(unittest.TestCase):
+
+    # TODO add tests for X730
+    def test_hello_world(self):
+        self.assertEqual("hello world!".upper(), "HELLO WORLD!")
