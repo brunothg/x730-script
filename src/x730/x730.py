fd9bd67432b2075d756bb263175c9cc1e8778427
brunothg <android.bruns@gmail.com>
2025-06-16 18:10:55 +0200

    x730 monitor _shutdown_status_pin

--- src/x730/x730.py
+++ src/x730/x730.py
@@ -1,8 +1,9 @@
 import logging
+import math
 import subprocess
-import threading
 import time
 from enum import Enum
+from typing import Self
 
 from gpiozero import DigitalInputDevice, DigitalOutputDevice
 
@@ -17,7 +18,10 @@ class Pins(Enum):
     BOOT = 17
 
 
-# TODO monitor _shutdown_status_pin
+def milliseconds() -> int:
+    return math.floor(time.monotonic() * 1000)
+
+
 class X730:
     """
     Singleton class for controlling the X730 expansion board.
@@ -26,8 +30,8 @@ class X730:
     """
     _LOG = logging.getLogger(__name__)
 
-    REBOOT_PULSE_MINIMUM = 200
-    REBOOT_PULSE_MAXIMUM = 600
+    REBOOT_PULSE_MINIMUM_MS = 200
+    REBOOT_PULSE_MAXIMUM_MS = 600
 
     def __new__(cls):
         singleton_attr_name = '_INSTANCE'
@@ -36,18 +40,66 @@ class X730:
         return getattr(cls, singleton_attr_name)
 
     def __init__(self):
-        self._lock = threading.RLock()
-        self._shutdown_button_pin = DigitalOutputDevice(pin=Pins.BUTTON.value, active_high=True, initial_value=False)
-        self._shutdown_status_pin = DigitalInputDevice(pin=Pins.SHUTDOWN.value, pull_up=False, bounce_time=None)
-        self._boot_status_pin = DigitalOutputDevice(pin=Pins.BOOT.value, active_high=True, initial_value=True)
+        self._opened: bool = False
+        self._boot_status_pin: DigitalOutputDevice | None = None
+        self._shutdown_button_pin: DigitalOutputDevice | None = None
+        self._shutdown_status_pin: DigitalInputDevice | None = None
+
+    def __enter__(self) -> Self:
+        self.open()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
 
     def _sys_poweroff(self):
         X730._LOG.info("System power off")
         subprocess.run('poweroff', check=True)
+        self.close()
 
     def _sys_reboot(self):
         X730._LOG.info("System reboot")
         subprocess.run('reboot', check=True)
+        self.close()
+
+    def _on_shutdown_status(self):
+        begin_ms = milliseconds()
+        self._shutdown_status_pin.wait_for_inactive(X730.REBOOT_PULSE_MAXIMUM_MS)
+        end_ms = milliseconds()
+        diff_ms = end_ms - begin_ms
+        X730._LOG.debug(f"Shutdown status: {diff_ms} ms")
+
+        if diff_ms > self.REBOOT_PULSE_MINIMUM_MS:
+            if diff_ms < self.REBOOT_PULSE_MAXIMUM_MS:
+                self._sys_reboot()
+            else:
+                self._sys_poweroff()
+
+    def open(self):
+        if not self._opened:
+            return
+        self._opened = True
+
+        self._boot_status_pin = DigitalOutputDevice(pin=Pins.BOOT.value, active_high=True, initial_value=True)
+        self._shutdown_button_pin = DigitalOutputDevice(pin=Pins.BUTTON.value, active_high=True, initial_value=False)
+
+        self._shutdown_status_pin = DigitalInputDevice(pin=Pins.SHUTDOWN.value, pull_up=False, bounce_time=None)
+        self._shutdown_status_pin.when_activated = self._on_shutdown_status
+
+
+    def close(self):
+        if not self._opened:
+            return
+        self._opened = False
+
+        self._boot_status_pin.close()
+        self._boot_status_pin = None
+
+        self._shutdown_button_pin.close()
+        self._shutdown_button_pin = None
+
+        self._shutdown_status_pin.close()
+        self._shutdown_status_pin = None
 
     def poweroff(self, force: bool = False) -> None:
         """
@@ -56,11 +108,10 @@ class X730:
         :param force: If True, force the power off (cut off the power).
         :return:
         """
-        with self._lock:
-            X730._LOG.info("Powering off board")
-            self._shutdown_button_pin.on()
-            time.sleep(5)
-            self._shutdown_button_pin.off()
+        X730._LOG.info("Powering off board")
+        self._shutdown_button_pin.on()
+        time.sleep(5)
+        self._shutdown_button_pin.off()
 
     def restart(self) -> None:
         """
@@ -68,8 +119,7 @@ class X730:
 
         :return:
         """
-        with self._lock:
-            X730._LOG.info("Restarting the board")
-            self._shutdown_button_pin.on()
-            time.sleep(1.5)
-            self._shutdown_button_pin.off()
+        X730._LOG.info("Restarting the board")
+        self._shutdown_button_pin.on()
+        time.sleep(1.2)
+        self._shutdown_button_pin.off()
