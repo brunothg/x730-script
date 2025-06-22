d6be82db9fdf9817630c73ce1593b8876a93d3b4
brunothg <android.bruns@gmail.com>
2025-06-22 21:40:39 +0200

    x730 tests

--- tests/test_x730/__init__.py
+++ tests/test_x730/__init__.py
@@ -1,7 +1,13 @@
+import logging
+
 from gpiozero import Device
 from gpiozero.pins.mock import MockFactory
 
+logging.basicConfig(
+    level=logging.DEBUG,
+)
+
 # Get Pi revision number: cat /proc/cpuinfo | grep "Revision"
 # More info at gpiozero.pins.pi.PiBoardInfo#from_revision
-pin_factory = MockFactory(revision='d03114') # Using 4B
+pin_factory = MockFactory(revision='d03114')  # Using 4B
 Device.pin_factory = pin_factory
--- tests/test_x730/test_x730.py
+++ tests/test_x730/test_x730.py
@@ -1,5 +1,9 @@
+import time
+from typing import cast
 from unittest import TestCase
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, Mock
+
+from gpiozero.pins.mock import MockPin
 
 from x730 import X730
 from . import pin_factory
@@ -9,24 +13,27 @@ class X730TestCase(TestCase):
 
     def setUp(self):
         pin_factory.reset()
-        self.x730 = X730()
 
-        x730 = self.x730
-        orig_poweroff = x730.poweroff
+        self.x730 = x730 = X730()
+        x730._sys_reboot = MagicMock()
+        x730._sys_poweroff = MagicMock()
+
+        def mock_shutdown_status(pulse: float):
+            x730._shutdown_status_pin.pin.drive_high()
+            time.sleep(pulse)
+            x730._shutdown_status_pin.pin.drive_low()
 
         def mock_poweroff(*args, **kwargs):
-            orig_poweroff(*args, **kwargs)
-            # TODO x730._shutdown_status_pin.pin.drive_high() # for t > REBOOT_PULSE_MAXIMUM
+            X730.poweroff(x730, *args, **kwargs)
+            mock_shutdown_status(X730.SHUTDOWN_STATUS_REBOOT_PULSE_MAXIMUM)
 
         x730.poweroff = MagicMock(side_effect=mock_poweroff)
 
-        orig_restart = x730.poweroff
-
         def mock_restart(*args, **kwargs):
-            orig_restart(*args, **kwargs)
-            # TODO x730._shutdown_status_pin.pin.drive_high() # for REBOOT_PULSE_MINIMUM > t < REBOOT_PULSE_MAXIMUM
+            X730.reboot(x730, *args, **kwargs)
+            mock_shutdown_status(X730.SHUTDOWN_STATUS_REBOOT_PULSE_MINIMUM)
 
-        x730.restart = MagicMock(side_effect=mock_restart)
+        x730.reboot = MagicMock(side_effect=mock_restart)
 
     def test_init(self):
         with self.x730 as device:
@@ -34,11 +41,34 @@ class X730TestCase(TestCase):
             self.assertTrue(device._opened)
 
     def test_power_off(self):
-        # TODO test_power_off (assert_states_and_times)
         with self.x730 as device:
             device.poweroff()
+            cast(MockPin, self.x730._shutdown_button_pin.pin).assert_states_and_times([
+                [0, False],
+                [0, True],
+                [X730.SHUTDOWN_BUTTON_POWEROFF_PULSE, False]
+            ])
+            cast(Mock, self.x730._sys_poweroff).assert_called_once()
+            cast(Mock, self.x730._sys_reboot).assert_not_called()
 
-    def test_restart(self):
-        # TODO test_power_off  (assert_states_and_times)
+    def test_power_off_crash(self):
         with self.x730 as device:
-            device.restart()
+            device.poweroff(force=True)
+            cast(MockPin, self.x730._shutdown_button_pin.pin).assert_states_and_times([
+                [0, False],
+                [0, True],
+                [X730.SHUTDOWN_BUTTON_CRASH_PULSE, False]
+            ])
+            cast(Mock, self.x730._sys_poweroff).assert_called_once()
+            cast(Mock, self.x730._sys_reboot).assert_not_called()
+
+    def test_reboot(self):
+        with self.x730 as device:
+            device.reboot()
+            cast(MockPin, self.x730._shutdown_button_pin.pin).assert_states_and_times([
+                [0, False],
+                [0, True],
+                [X730.SHUTDOWN_BUTTON_REBOOT_PULSE, False]
+            ])
+            cast(Mock, self.x730._sys_reboot).assert_called_once()
+            cast(Mock, self.x730._sys_poweroff).assert_not_called()
